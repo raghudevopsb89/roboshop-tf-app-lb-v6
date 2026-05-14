5c8d01b312f9efa1f8919793d2f4ba00d69c2342
Raghu K <raghuk.vit@gmail.com>
2026-05-14 06:23:10 +0530

    RoboShop Code

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "${var.component_name}-${var.env}-nic"
-    subnet_id                     = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/rhel10-vmVNET/subnets/rhel10-vmSubnet"
+    subnet_id                     = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/workstation-vnet/subnets/default"
     private_ip_address_allocation = "Dynamic"
   }
 }
