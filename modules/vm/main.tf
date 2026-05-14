ca60715512a925e067a8059568a3501224e861c6
Raghu K <raghuk.vit@gmail.com>
2026-05-14 06:55:58 +0530

    RoboShop Code

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -62,6 +62,7 @@ resource "azurerm_lb_backend_address_pool_address" "main" {
   name                                = "${var.component_name}-${var.env}"
   backend_address_pool_id             = azurerm_lb_backend_address_pool.main[0].id
   ip_address                          = azurerm_network_interface.main.private_ip_address
+  virtual_network_id                  = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/workstation-vnet/subnets/default"
 }
 
 output "lb" {
